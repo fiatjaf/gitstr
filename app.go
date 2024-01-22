61db967512dba99cba23388f4a1a4bef7348c081
fiatjaf <fiatjaf@gmail.com>
2024-01-22 16:01:20 -0300

    move everything to the top-level and leave just a basic initialization thing at cmd/git-str

---
+++ app.go
@@ -0,0 +1,32 @@
+package gitstr
+
+import (
+	"context"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/urfave/cli/v3"
+)
+
+var pool *nostr.SimplePool
+
+const (
+	RepoAnnouncementKind = 30617
+	PatchKind            = 1617
+	IssueKind            = 1621
+	ReplyKind            = 1622
+)
+
+var App = &cli.Command{
+	Name:                   "git str",
+	Suggest:                true,
+	UseShortOptionHandling: true,
+	Before: func(ctx context.Context, c *cli.Command) error {
+		pool = nostr.NewSimplePool(ctx)
+		return nil
+	},
+	Commands: []*cli.Command{
+		initRepo,
+		download,
+		send,
+	},
+}
--- cmd/git-str/main.go
+++ cmd/git-str/main.go
@@ -5,36 +5,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/nbd-wtf/go-nostr"
-	"github.com/urfave/cli/v3"
+	"github.com/fiatjaf/gitstr"
 )
-
-var pool *nostr.SimplePool
-
-const (
-	RepoAnnouncementKind = 30617
-	PatchKind            = 1617
-	IssueKind            = 1621
-	ReplyKind            = 1622
-)
-
-var app = &cli.Command{
-	Name:                   "git str",
-	Suggest:                true,
-	UseShortOptionHandling: true,
-	Before: func(ctx context.Context, c *cli.Command) error {
-		pool = nostr.NewSimplePool(ctx)
-		return nil
-	},
-	Commands: []*cli.Command{
-		initRepo,
-		download,
-		send,
-	},
-}
 
 func main() {
-	if err := app.Run(context.Background(), os.Args); err != nil {
+	if err := gitstr.App.Run(context.Background(), os.Args); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
